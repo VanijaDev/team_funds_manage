99d1c9af1f4eed8073ae5dd2ff0691e8009c96d1
Ivan Solomichev <vanijatko.social@gmail.com>
2017-12-11 18:28:32 +0200

    Initial migrations fixed

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 var Migrations = artifacts.require("./Migrations.sol");
+var TeamWallet = artifacts.require("./TeamWallet.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(TeamWallet, 'Cool Team', {value: 100});
 };
