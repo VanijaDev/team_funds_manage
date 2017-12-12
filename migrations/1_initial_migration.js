00fa5f5376fef7cac4e6a6f6fbc5a084b6ba8951
Ivan Solomichev <vanijatko.social@gmail.com>
2017-12-12 15:29:02 +0200

    updated currency for test

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -3,5 +3,5 @@ var TeamWallet = artifacts.require("./TeamWallet.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
-  deployer.deploy(TeamWallet, 'Cool Team', {value: web3.toWei(0.1, 'ether')});
+  deployer.deploy(TeamWallet, 'Cool Team', {value: web3.toWei(0.1, 'finney')});
 };
